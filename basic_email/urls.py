0e44d3d06d3758af7cfe3ac902aa1ea4f3d107e8
Jacek <jacek@ivolution.pl>
2015-01-19 17:17:40 +0100

    AA-730 copied over from earnest

--- basic_email/urls.py
+++ basic_email/urls.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+from basic_email.views import FakeEmailSend
+from django.contrib.admin.views.decorators import staff_member_required
+
+
+urlpatterns = patterns('',
+                       url(r"$", staff_member_required(FakeEmailSend.as_view())),
+                       )
