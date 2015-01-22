0a23d38e25d945dfbfaabcf884899bb0ea7da943
Jacek <jacek@ivolution.pl>
2015-01-22 16:39:11 +0100

    AA-730 listing template variables in progress

--- basic_email/views.py
+++ basic_email/views.py
@@ -38,13 +38,52 @@ class ListEmailTemplatesView(TemplateView):
         return context
 
 
-class PreviewEmailView(TemplateView):
+class ListEmailVariables(TemplateView):
 
-    def get_template_names(self, *args, **kwargs):
+    def get_template(self, *args, **kwargs):
         if not self.request.GET.get('template'):
             raise NameError("?template=... is required")
-        return [os.path.join(settings.BASIC_EMAIL_DIRECTORY, self.request.GET.get('template'))]
+        return os.path.join(settings.BASIC_EMAIL_DIRECTORY, self.request.GET.get('template'))
+
+    def get_template_names(self, *args, **kwargs):
+        return [self.get_template()]
+
+    def get_template_data(self):
+        from django.template.loader import get_template
+        template_data = get_template(self.get_template())
+        return template_data
+
+    def content_encode(self, content):
+        content = content.replace('{{', '{##_')
+        return content
+
+    def contend_decode(self, content):
+        content = content.replace('{##', '{{')
+        return content
+
+    def list_template_variables(self):
+        template_data = self.get_template_data()
+        files = []
+        for n in template_data.nodelist:
+            # print ('filename in loop')
+            # print n.get_parent(None).origin
+            # print n.source[0]
+            files.append('%s' % n.get_parent(None).origin)
+        files.append('%s' % template_data.origin)
+        f = open(self.dir + file_name, 'r')
+        # save in tmp using random name: https://docs.python.org/2/library/tempfile.html
+        # add this tmp directory to settings to search for template there. 
+        content = f.read()
+        content = self.content_encode(content)
+        tmp_f = open(self.tmp_dir + file_name, 'w+')
+        tmp_f.write(content)
+        tmp_f.close()
+        print files
+
+
+class PreviewEmailView(ListEmailVariables):
 
     def get_context_data(self, *args, **kwargs):
         context = super(PreviewEmailView, self).get_context_data(*args, **kwargs)
+        self.list_template_variables()
         return context
