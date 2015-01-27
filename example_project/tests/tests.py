1a0b74b8289245ea67bf9ddf961b2da8fe43db9a
Jacek <jacek@ivolution.pl>
2015-01-27 14:03:40 +0100

    AA-730 update tests

--- example_project/tests/tests.py
+++ example_project/tests/tests.py
@@ -15,51 +15,21 @@ class EmailsTests(TestCase):
         self.admin = User.objects.create_superuser('admin', 'admin@test.com', self.password)
         self.client = Client()
 
-    def test_send(self):
-        send_email('emails/email_start.html', 'foo@bar.com', 'Lorem')
-        self.assertEqual(len(mail.outbox), 1)
-
-    def test_list(self):
+    def _test_access(self, url_name, template):
         # access page when not logged in - should throw an error
-        response = self.client.get(reverse('list-email-templates'), follow=True)
-        self.assertTemplateNotUsed(response, 'admin/list.html')
+        response = self.client.get(reverse(url_name), follow=True)
+        self.assertTemplateNotUsed(response, template)
 
-        # access by admin
-        self.client.login(password=self.password, username=self.admin.username)
-        response = self.client.get(reverse('list-email-templates'))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context[0].get('files')), 1)
-
-    def test_preview(self):
-        response = self.client.get(reverse('preview-email-template'), follow=True)
-        self.assertTemplateNotUsed(response, 'admin/preview.html')
-
+    def _test_required_get_param_and_template_existence(self, url_name):
         # trying to access w/o template parameter will raise an error
         self.client.login(password=self.password, username=self.admin.username)
-        self.assertRaises(NameError, self.client.get, reverse('preview-email-template'))
+        self.assertRaises(NameError, self.client.get, reverse(url_name))
 
         # not existing template
         self.assertRaises(TemplateDoesNotExist, self.client.get,
-                          reverse('preview-email-template') + '?template=non_existing_fail')
+                          reverse(url_name) + '?template=non_existing_fail')
 
-        # proper
-        response = self.client.get(reverse('preview-email-template') + '?template=email_start')
-        self.assertEqual(response.status_code, 200)
-
-    def test_variables_list(self):
-        response = self.client.get(reverse('list-email-template-variables'), follow=True)
-        self.assertTemplateNotUsed(response, 'admin/list-template-variables.html')
-
-        # trying to access w/o template parameter will raise an error
-        self.client.login(password=self.password, username=self.admin.username)
-        self.assertRaises(NameError, self.client.get, reverse('list-email-template-variables'))
-
-        # not existing template
-        self.assertRaises(TemplateDoesNotExist, self.client.get,
-                          reverse('list-email-template-variables') + '?template=non_existing_fail')
-
-        # proper
-        response = self.client.get(reverse('list-email-template-variables') + '?template=email_start')
+    def _test_variables_list_response(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['template_variables']), 7)
         self.assertEqual(response.context['template_variables'][0], 'emailSubject')
@@ -69,3 +39,46 @@ class EmailsTests(TestCase):
         self.assertEqual(response.context['template_variables'][4], 'test_template_variable_3')
         self.assertEqual(response.context['template_variables'][5], 'test_template_variable_4')
         self.assertEqual(response.context['template_variables'][6], 'test_template_variable_5')
+
+    def test_send(self):
+        send_email('emails/email_start.html', 'foo@bar.com', 'Lorem')
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_list(self):
+        self._test_access('list-email-templates', 'admin/list.html')
+        # access by admin
+        self.client.login(password=self.password, username=self.admin.username)
+        response = self.client.get(reverse('list-email-templates'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context[0].get('files')), 1)
+
+    def test_preview(self):
+        self._test_access('preview-email-template', 'admin/preview.html')
+        self._test_required_get_param_and_template_existence('preview-email-template')
+
+        # proper
+        response = self.client.get(reverse('preview-email-template') + '?template=email_start')
+        self.assertEqual(response.status_code, 200)
+
+    def test_variables_list(self):
+        self._test_access('list-email-template-variables', 'admin/list.html')
+        self._test_required_get_param_and_template_existence('list-email-template-variables')
+
+        # proper
+        response = self.client.get(reverse('list-email-template-variables') + '?template=email_start')
+        self._test_variables_list_response(response)
+
+    def test_sending_email_preview(self):
+        self._test_access('send-email-preview', 'admin/email-form.html')
+        self._test_required_get_param_and_template_existence('send-email-preview')
+
+        response = self.client.get(reverse('send-email-preview') + '?template=email_start')
+        self._test_variables_list_response(response)
+
+        data = {
+            'email': 'foo@bar.com'
+        }
+        response = self.client.post(reverse('send-email-preview') + '?template=email_start', data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['success'])
+        self.assertEqual(len(mail.outbox), 1)
